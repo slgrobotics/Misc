4e93e457c1c305c9a97aa181a2db1bde62589680
Sergei Grichine <msghub@hotmail.com>
2025-10-14 11:53:39 -0500

    Auto formatted FOC velocity control example

--- Arduino/Sketchbook/FOC_velocity_control_HALL/FOC_velocity_control_HALL.ino
+++ Arduino/Sketchbook/FOC_velocity_control_HALL/FOC_velocity_control_HALL.ino
@@ -1,21 +1,21 @@
 /**
- *
- * Velocity motion control example
- * Steps:
- * 1) Configure the motor and sensor
- * 2) Run the code
- * 3) Set the target velocity (in radians per second) from serial terminal
- *
- *
- *
- * NOTE :
- * > Specifically for Arduino UNO example code for running velocity motion control using a hall sensor
- * > Since Arduino UNO doesn't have enough interrupt pins we have to use software interrupt library PciManager.
- *
- * > If running this code with Nucleo or Bluepill or any other board which has more than 2 interrupt pins
- * > you can supply doC directly to the sensor.enableInterrupts(doA,doB,doC) and avoid using PciManger
- *
- */
+
+   Velocity motion control example
+   Steps:
+   1) Configure the motor and sensor
+   2) Run the code
+   3) Set the target velocity (in radians per second) from serial terminal
+
+
+
+   NOTE :
+   > Specifically for Arduino UNO example code for running velocity motion control using a hall sensor
+   > Since Arduino UNO doesn't have enough interrupt pins we have to use software interrupt library PciManager.
+
+   > If running this code with Nucleo or Bluepill or any other board which has more than 2 interrupt pins
+   > you can supply doC directly to the sensor.enableInterrupts(doA,doB,doC) and avoid using PciManger
+
+*/
 #include <SimpleFOC.h>
 // software interrupt library
 #include <PciManager.h>
@@ -24,7 +24,7 @@
 // miniPRO wheels are very low resistance, "1" is fine:
 #define DRIVER_VOLTAGE_LIMIT 3
 #define POWER_SUPPLY_VOLTAGE 12
-#define MOTOR_VOLTAGE_ALIGN 2
+#define MOTOR_VOLTAGE_ALIGN 0.5
 #define POLE_PAIRS 15
 
 // BLDC motor & driver instance
@@ -36,9 +36,18 @@ HallSensor sensor = HallSensor(2, 3, 4, POLE_PAIRS);
 
 // Interrupt routine intialization
 // channel A and B callbacks
-void doA(){sensor.handleA();}
-void doB(){sensor.handleB();}
-void doC(){sensor.handleC();}
+void doA() {
+  //Serial.println("doA");
+  sensor.handleA();
+}
+void doB() {
+  //Serial.println("doB");
+  sensor.handleB();
+}
+void doC() {
+  //Serial.println("doC");
+  sensor.handleC();
+}
 // If no available hadware interrupt pins use the software interrupt
 PciListenerImp listenerIndex(sensor.pinC, doC);
 
@@ -46,33 +55,47 @@ PciListenerImp listenerIndex(sensor.pinC, doC);
 float target_velocity = 0;
 // instantiate the commander
 Commander command = Commander(Serial);
-void doTarget(char* cmd) { command.scalar(&target_velocity, cmd); }
+void doTarget(char* cmd) {
+  command.scalar(&target_velocity, cmd);
+}
 
 void setup() {
 
-  // use monitoring with serial 
+  // use monitoring with serial
   Serial.begin(115200);
   // enable more verbose output for debugging
   // comment out if not needed
-  SimpleFOCDebug::enable(&Serial);
+  //SimpleFOCDebug::enable(&Serial);
 
-  // initialize sensor sensor hardware
-  sensor.init();
+  // initialize HALL sensor hardware
+  sensor.init(); // void sub
+
   sensor.enableInterrupts(doA, doB); //, doC);
-  // software interrupts
+  // third software interrupt for Arduino UNO:
   PciManager.registerListener(&listenerIndex);
+
   // link the motor to the sensor
   motor.linkSensor(&sensor);
 
   // driver config
   // power supply voltage [V]
   driver.voltage_power_supply = POWER_SUPPLY_VOLTAGE;
-  driver.init();
+  driver.voltage_limit = DRIVER_VOLTAGE_LIMIT;
+
+  if (!driver.init()) {
+    Serial.println("Driver init failed!");
+    return;
+  }
+
   // link the motor and the driver
   motor.linkDriver(&driver);
 
-  // aligning voltage [V]
+  // During the sensor align procedure, SimpleFOC moves the wheels and measures
+  //     the direction of the sensor and the zero electrical angle offset.
+  // set aligning voltage [Volts]:
   motor.voltage_sensor_align = MOTOR_VOLTAGE_ALIGN;
+  // index search velocity [rad/s]
+  motor.velocity_index_search = 3;
 
   // set motion control loop to be used
   motor.controller = MotionControlType::velocity;
@@ -80,12 +103,14 @@ void setup() {
   // contoller configuration
   // default parameters in defaults.h
 
-  // velocity PI controller parameters
+  // velocity PID controller parameters
   motor.PID_velocity.P = 0.2f;
   motor.PID_velocity.I = 2;
   motor.PID_velocity.D = 0;
-  // default voltage_power_supply
+
+  // initial motor voltage limit
   motor.voltage_limit = DRIVER_VOLTAGE_LIMIT;
+
   // jerk control using voltage voltage ramp
   // default value is 300 volts per sec  ~ 0.3V per millisecond
   motor.PID_velocity.output_ramp = 1000;
@@ -94,18 +119,18 @@ void setup() {
   motor.LPF_velocity.Tf = 0.01f;
 
   // comment out if not needed
-  motor.useMonitoring(Serial);
+  //motor.useMonitoring(Serial);
 
   Serial.println("IP: initializing motor and FOC...");
 
   // initialize motor
-  if(!motor.init()) {
+  if (!motor.init()) {
     Serial.println("Motor init failed!");
     return;
   }
-  
+
   // align sensor and start FOC
-  if(!motor.initFOC()) {
+  if (!motor.initFOC()) {
     Serial.println("FOC init failed!");
     return;
   }
